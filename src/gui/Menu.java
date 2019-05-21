55eca68b3e636d8facdbe1abb558a784655e2673
dani_ <dani_@Dani-Torre>
2019-05-21 09:53:26 +0200

    Abrir programador de tareas

--- src/gui/Menu.java
+++ src/gui/Menu.java
@@ -1,7 +1,15 @@
 package gui;
 
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JDialog;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 public class Menu extends JMenuBar {
 
@@ -9,9 +17,29 @@ public class Menu extends JMenuBar {
 	public Menu () {
 		
 		JMenu menu = new JMenu("Abrir");
-		menu.add("Programador de Tareas");
+		menu.add(crearMenuItemLanzamientoPanelGestionComoJDialog("Programador de tareas", new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				try 
+				{ 
+				   /* directorio/ejecutable es el path del ejecutable y un nombre */ 
+				   Process p = Runtime.getRuntime().exec ("cmd /c %windir%\\system32\\taskschd.msc /s"); 
+				} 
+				catch (Exception e) 
+				{ 
+				   JOptionPane.showMessageDialog(null, "No se ha podido abrir el programador de Tareas");
+				}			
+			}
+		}));
 		this.add(menu);
 		
+	}
+	
+	private JMenuItem crearMenuItemLanzamientoPanelGestionComoJDialog (String titulo, ActionListener actionListener) {
+		JMenuItem mi = new JMenuItem(titulo);
+		mi.addActionListener(actionListener);
+		return mi;
 	}
 	
 }
